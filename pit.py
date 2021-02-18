f307114a3184b5a17676e6e35fda215ffe2f3941
Simon <simon.schnecko@mnd.thm.de>
2021-02-18 14:18:59 +0100

    new file:   Mills/MillsGame.py 	new file:   Mills/MillsLogic.py 	new file:   Mills/MillsPlayers.py 	new file:   Mills/__init__.py 	new file:   Mills/keras/MillsNNet.py 	new file:   Mills/keras/NNet.py 	new file:   Mills/keras/__init__.py 	modified:   main.py 	modified:   pit.py 	modified:   tictactoe/keras/NNet.py

--- pit.py
+++ pit.py
@@ -26,7 +26,7 @@ hp = HumanTicTacToePlayer(g).play
 # nnet players
 n1 = NNet(g)
 
-n1.load_checkpoint('./pretrained_models/tictactoe/keras/','best-25eps-25sim-10epch.pth.tar')
+n1.load_checkpoint('./temp2/', 'best.pth.tar')#('./pretrained_models/tictactoe/keras/','best-25eps-25sim-10epch.pth.tar')
 args1 = dotdict({'numMCTSSims': 50, 'cpuct':1.0})
 mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
@@ -35,7 +35,7 @@ if human_vs_cpu:
     player2 = hp
 else:
     n2 = NNet(g)
-    n2.load_checkpoint('./pretrained_models/tictactoe/keras/','best-25eps-25sim-10epch.pth.tar')
+    n2.load_checkpoint('./temp2/', 'best.pth.tar')
     args2 = dotdict({'numMCTSSims': 50, 'cpuct': 1.0})
     mcts2 = MCTS(g, n2, args2)
     n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
