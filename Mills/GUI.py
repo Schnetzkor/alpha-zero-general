6cba3e43a572b1f63fc1dae1b2514a924cf70bbe
Philipp Petermeier <philipp.petermeier@mnd.thm.de>
2021-02-26 15:04:52 +0100

    Brett & Leere Positionen zeichnen funktioniert sowie Rückumwandlung von Klicks auf Index. Todo: Einen Stein in der Matrix ablegen, um dann die entsprechenden Kreise für die Spieler zeichnen zu können. Danach: legal_moves übergeben lassen um diese zeichnen zu können.

--- Mills/GUI.py
+++ Mills/GUI.py
@@ -45,36 +45,55 @@ def getscale():
     return scale
 
 
+def testboard():
+    testboard = np.zeros((3, 8))
+    return testboard
+
+
+# def write_coordinates(board):
+#    coordinates = np.copy(board)
+#    dimension = 1
+#    for i in range(len(coordinates)):
+#        dimension + 4
+#    vdistance = 2, 0
+#    hdistance = 0, 2
+#    cdistance = 2, 0
+#    anchor1 = (dimension / 2) - 2
+#    anchor2 = dimension / 2
+#    anchor =np.array([anchor1, anchor2])
+#    anchor[0] = int(dimension / 2) - 2), int(dimension / 2)
+#    counter = anchor
+#    for i in range(len(coordinates)):
+#        for j in range(len(coordinates[i])):
+#            if i == 0 and j == 0:
+#                coordinates[i[j]] = int(anchor1), int(anchor2)
+#                break
+#            if i > 0 == True and j == 0:
+#                counter = anchor - int(i * cdistance)
+#                coordinates[i[j]] = counter
+#            if j == 1:
+#                counter = counter + hdistance
+#                coordinates[i[j]] = counter
+#                break
+#            if j < 4 == True:
+#                counter = counter + vdistance
+#                coordinates[i[j]] = counter
+#                break
+#            if j < 6 == True:
+#                counter = counter - hdistance
+#                coordinates[i[j]] = counter
+#                break
+#            else:
+#                counter = counter - vdistance
+#        vdistance[0] + 1, hdistance[1] + 1
+#    return coordinates
+
+
 coordinates = np.array([np.array([(5, 7), (5, 9), (7, 9), (9, 9), (9, 7), (9, 5), (7, 5), (5, 5)]),
                         np.array([(3, 7), (3, 11), (7, 11), (11, 11), (11, 7), (11, 3), (7, 3), (3, 3)]),
                         np.array([(1, 7), (1, 13), (7, 13), (13, 13), (13, 7), (13, 1), (7, 1), (1, 1)])])
-# coordinates[0[1]] = pos1
-# coordinates[0[2]] = 7, 9
-# coordinates[0[3]] = 9, 9
-# coordinates[0[4]] = 9, 7
-# coordinates[0[5]] = 9, 5
-# coordinates[0[6]] = 7, 5
-# coordinates[0[7]] = 5, 5
-# coordinates[1[0]] = 3, 7
-# coordinates[1[1]] = 3, 11
-# coordinates[1[2]] = 7, 11
-# coordinates[1[3]] = 11, 11
-# coordinates[1[4]] = 11, 7
-# coordinates[1[5]] = 11, 3
-# coordinates[1[6]] = 7, 3
-# coordinates[1[7]] = 3, 3
-# coordinates[2[0]] = 1, 7
-# coordinates[2[1]] = 1, 13
-# coordinates[2[2]] = 7, 13
-# coordinates[2[3]] = 13, 13
-# coordinates[2[4]] = 13, 7
-# coordinates[2[5]] = 13, 1
-# coordinates[2[6]] = 7, 1
-# coordinates[2[7]] = 1, 1
 
-coordinates=coordinates*scale
-
-print(coordinates)
+coordinates = coordinates * scale
 
 
 # def getcoordinates(board):
@@ -91,7 +110,7 @@ print(coordinates)
 #    coordinates[1[0]] = (3, 7)
 #    coordinates[1[1]] = (3, 11)
 #    coordinates[1[2]] = (7, 11)
-#   coordinates[1[3]] = (11, 11)
+#    coordinates[1[3]] = (11, 11)
 #    coordinates[1[4]] = (11, 7)
 #    coordinates[1[5]] = (11, 3)
 #    coordinates[1[6]] = (7, 3)
@@ -106,21 +125,23 @@ print(coordinates)
 #    coordinates[2[7]] = (1, 1)
 
 
-def get_index(gui_position):
-    gui_position = gui_position / scale
-    indexfound = np.where(coordinates == gui_position)
+def get_index(gui_position1, gui_position2):
+    gui_position1 = gui_position1 / scale
+    gui_position2 = gui_position2 / scale
+    indexfound = np.where(coordinates == gui_position1, gui_position2)
     return indexfound
 
 
-def get_gui_position(index_position):
-    ui_position = coordinates[index_position[0], index_position[1]]
-    ui_position * scale
-    return ui_position
+def get_gui_position(index_position1, index_position2):
+    gui_position = coordinates[index_position1, index_position2]
+    gui_position * scale
+    return gui_position
 
 
 ##------------Farbdefinition-----------------------------
 BLACK, GREEN, BLUE, GRAY, YELLOW, WHITE = (0, 0, 0), (0, 255, 0), (0, 0, 255), (127, 127, 127), (255, 255, 0), (
     255, 255, 255)
+
 
 # BLACK=(0, 0, 0)
 # GREEN = (0, 255, 0)
@@ -128,8 +149,6 @@ BLACK, GREEN, BLUE, GRAY, YELLOW, WHITE = (0, 0, 0), (0, 255, 0), (0, 0, 255), (
 # GRAY = (127, 127, 127)
 # YELLOW = (255, 255, 0)
 # WHITE = (255, 255, 255)
-
-
 
 
 ##-------------- Darstellungserstellung ----------------
@@ -141,7 +160,7 @@ def drawbackground():
 def drawlines():
     for i in range(3):
         for j in range(8):
-            start, finish = coordinates[i][j], coordinates[i][((j+1) % 8)]
+            start, finish = coordinates[i][j], coordinates[i][((j + 1) % 8)]
             pygame.draw.line(screen, BLACK, start, finish, line)
             if j % 2 == 0 and i == 0:
                 start, finish = coordinates[i][j], coordinates[(i + 2)][j]
@@ -150,36 +169,41 @@ def drawlines():
 
 
 ## Soll nach jedem Zug aufgerufen werden, sofern nicht jedes Mal das Board komplett neu gezeichnet werden muss? Wenn nein, werden hier nur die Veraenderungen angezeigt
-def updateboard(board, player):
-    for i in (range(board)):  # -1 am Ende, Extravariabeln!
-        for j in range(board[i]):
-            valid = get_legal_moves(player)
-            if board[i, j] == valid:
-                pygame.draw.circle(surface=screen, color=GREEN, center=(get_gui_position(i, j)), radius=(scale / 2))
-                pygame.display.update()
-                break
+def updateboard(board):  # muss noch player hin
+    for i in range(3):  # -1 am Ende, Extravariabeln!
+        for j in range(8):
+            # valid = get_legal_moves(player)
+            # if board[i, j] == valid:
+            #    pygame.draw.circle(surface=screen, color=GREEN, center=(get_gui_position(i, j)), radius=(scale / 2))
+            #    pygame.display.update()
+            #    break
             if board[i, j] == 1:
                 pygame.draw.circle(surface=screen, color=YELLOW, center=(get_gui_position(i, j)), radius=(scale / 2))
                 pygame.display.update()
-                break
-            if board[i, j] == -1:
-                pygame.draw.circle(surface=screen, color=BLUE, center=(get_gui_position(i, j)), radius=(scale / 2))
-                pygame.display.update()
-                break
+            # if board[i, j] == -1:
+            #    pygame.draw.circle(surface=screen, color=BLUE, center=(get_gui_position(i, j)), radius=(scale / 2))
+            #    pygame.display.update()
+            #    break
             else:
                 pygame.draw.circle(surface=screen, color=GRAY, center=(get_gui_position(i, j)), radius=(scale / 2))
                 pygame.display.update()
-                break
+            #    break
 
 
 ## ------------------------- Initialisierungsvariabeln -------------------------------
+board = np.zeros((3, 8))
 width: int = 14 * scale
 height: int = 14 * scale
-size = [width, height]
-screen = pygame.display.set_mode(size=size)
+size = (width, height)
 pygame.init()
+screen = pygame.display.set_mode(size)
+myfont = pygame.font.SysFont("monospace", 75)
+# board = write_coordinates(board)
+print(coordinates)
+print(board)
 drawbackground()
 drawlines()
+updateboard(board)
 pygame.display.update()
 pygame.time.wait(30000)
 ##--------------------------- Testinitialisierung -----------------------------------
@@ -189,24 +213,38 @@ pygame.time.wait(30000)
 
 
 ## --------------Eventsteuerung, entnommen und angepasst aus Uebung 3 --------------
-# while board.pieces[3, 7] == 0:  ##piece [3,7] speichert Spielzustand
-#   if event.type == pygame.QUIT:
-#        sys.exit()
-#    if player_turn == TRUE:
-#        for event in pygame.event.get():
-#            if event.type == pygame.MOUSEBUTTONDOWN:
-#                posy = event.pos[0]
-#                posx = event.pos[1]
-#                position = [int(math.floor(posy / scale)), int(math.floor(posx / scale))]
-#                # column = int(math.floor(posy / scale))
-#                # row = int(math.floor(posx / scale))
-#                valid = self.game.getValidMoves(board, player)
-#                for i in range(valid):
-#                    if position == valid(i):
-#                        execute_move(position)
-#                        updateboard(board)
-#                        pygame.display.update()
-#                       break
+# while board.pieces[3, 7] == 0:  ##piece [3,7]: speichert Spielzustand
+player_turn = True
+player = 1
 
+def set_piece(board, player, col, row):
+    piece = player
+    loc_col=col
+    loc_row=row
+    board[col, row] = 1
+    return board
+
+while player_turn:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            posy = event.pos[0]
+            posx = event.pos[1]
+            col = int(round(posx / scale))
+            row = int(round(posy / scale))
+            print(col, row)
+            #set_piece(board, player, col, row)
+            updateboard(board)
+            #position = [int(math.floor(posy / scale)), int(math.floor(posx / scale))]
+
+            # column = int(math.floor(posy / scale))
+            # row = int(math.floor(posx / scale))
+            # valid = self.game.getValidMoves(board, player)
+            # for i in range(valid):
+            #    if position == valid(i):
+            #        execute_move(position)
+            #        updateboard(board)
+            #        pygame.display.update()
+            #        break
+    pygame.display.update()
 # else:
 # einfach update aufrufen wenn der zug gemacht wird?
