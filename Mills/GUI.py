1703c8d00060bc289bf9b331fa0d2be457ce7124
Philipp Petermeier <philipp.petermeier@mnd.thm.de>
2021-02-28 20:56:38 +0100

    Koordinate gespiegelt, um dem Brett zu entsprechen

--- Mills/GUI.py
+++ Mills/GUI.py
@@ -103,7 +103,7 @@ coordinates = np.array([[(5, 7), (5, 9), (7, 9), (9, 9), (9, 7), (9, 5), (7, 5),
                        [(3, 7), (3, 11), (7, 11), (11, 11), (11, 7), (11, 3), (7, 3), (3, 3)],
                         [(1, 7), (1, 13), (7, 13), (13, 13), (13, 7), (13, 1), (7, 1), (1, 1)]])
 
-coordinates = np.roll(np.fliplr(coordinates), 7 , 1)
+#coordinates = np.roll(np.fliplr(coordinates),  , 1)
 
 scaled_coordinates = coordinates * scale
 
@@ -143,6 +143,7 @@ def get_index(gui_position1, gui_position2):     ## Das selbsterstellte Board ha
     search = (gui_position1, gui_position2)
     rows = coordinates.shape[0]
     cols = coordinates.shape[1]
+    print(search)
     for i in range(rows):
         for j in range(cols):
             if np.all(coordinates[i, j] == search):
@@ -218,7 +219,7 @@ pygame.time.wait(30000)
 ## --------------Eventsteuerung, entnommen und angepasst aus Uebung 3 --------------
 # while board.pieces[3, 7] == 0:  ##piece [3,7]: speichert Spielzustand
 player_turn = True
-player = -1
+player = 1
 
 def set_piece(board, player, col, row):
     place = get_index(col, row)
